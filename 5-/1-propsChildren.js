52bd48dc85a09348e2a2b67067ae22d811e2e494
saloum-sylla <127609294+saloum-sylla@users.noreply.github.com>
2023-09-15 13:00:46 +0200

    Add files via upload

---
+++ 5-/1-propsChildren.js
@@ -0,0 +1,8 @@
+/*
+Faites un nouveau composant appelé Boite.
+Dans votre composant App, placez le composant Boite comme une balise, 
+semblable à comment vous utiliseriez une balise HTML , et écrivez un texte entre ces balises.
+<Boite>blabla</Boite>
+Observez le résultat. Qu'est-ce que vous voyez à l'écran?
+inspectez le composant avec react developer tools
+*/
