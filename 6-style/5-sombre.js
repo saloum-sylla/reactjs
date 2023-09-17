5c82769b2fdcf121cfcdb3fd83f784d55272f174
saloum-sylla <127609294+saloum-sylla@users.noreply.github.com>
2023-09-17 17:56:14 +0200

    5-sombre.js

--- 6-style/5-sombre.js
+++ 6-style/5-sombre.js
@@ -1,6 +1,7 @@
 /**
- * Créez un composant qui permet à l'utilisateur de basculer entre un thème sombre et un thème clair.
-Créez deux fichiers CSS externes, un pour chaque thème.
-Lorsque l'utilisateur bascule entre les thèmes, 
-chargez dynamiquement le fichier CSS approprié pour changer le style de l'application.
+Créez un composant React qui permet à l'utilisateur de basculer entre un thème sombre et un thème clair.
+Créez un fichier CSS externe contenant les styles appropriés pour les deux thèmes.
+Lorsque l'utilisateur bascule entre les thèmes en cliquant sur un bouton,
+utilisez JavaScript pour ajouter ou supprimer les classes CSS correspondant au thème actuel sur l'ensemble de la page,
+ce qui changera dynamiquement le style de l'application en fonction du thème sélectionné.
  */
