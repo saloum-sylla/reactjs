91434cbdc601749e252cc62c59da2d6204e8ecc1
saloum-sylla <127609294+saloum-sylla@users.noreply.github.com>
2023-09-22 09:20:23 +0200

    Update 0-react-router.js

--- 08-router/0-react-router.js
+++ 08-router/0-react-router.js
@@ -1,63 +1,66 @@
 /*
-https://v5.reactrouter.com/web/guides/quick-start
+React Router est une bibliothèque de routage pour les applications React, 
+permettant de gérer la navigation entre différentes vues de manière fluide et personnalisable.
 
-React Router est une bibliothèque de routage pour les applications React. 
-Elle permet de gérer la navigation entre les différentes vues de votre application,
-en offrant une expérience fluide et personnalisable pour les utilisateurs.
-Pour commencer à utiliser React Router, vous devez installer la bibliothèque en utilisant la commande suivante :
-    npm install react-router-dom
-Une fois la bibliothèque installée, vous pouvez commencer à l'utiliser dans votre application. 
+Installation :
+Pour utiliser React Router, commencez par l'installer dans votre projet en exécutant la commande 
+`npm install react-router-dom` dans le terminal à la racine du projet.
 
-Voici un exemple de configuration de base :
-    import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-    import Home from './components/Home';
-    import About from './components/About';
-    import Contact from './components/Contact';
+Configuration de Base :
+Une fois la bibliothèque installée, vous pouvez l'intégrer dans votre application. 
+Créez un composant racine qui enveloppe votre application avec le composant Router et utilisez le composant Routes pour définir les routes :
 
-    function App() {
-        return (
-            <Router>
-            <Switch>
-                <Route exact path="/" component={Home} />
-                <Route path="/about" component={About} />
-                <Route path="/contact" component={Contact} />
-            </Switch>
-            </Router>
-        );
-        }
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"; // Importation des composants de React Router
+import Home from './components/Home'; // Importation du composant Home
+import About from './components/About'; // Importation du composant About
+import Contact from './components/Contact'; // Importation du composant Contact
 
-    export default App;
+function App() {
+  return (
+    <Router> // Composant racine de React Router
+      <Routes> // Composant pour définir les routes
+        <Route path="/" element={<Home />} /> // Route pour la page d'accueil
+        <Route path="/about" element={<About />} /> // Route pour la page "À propos"
+        <Route path="/contact" element={<Contact />} /> // Route pour la page de contact
+      </Routes>
+    </Router>
+  );
+}
 
-Dans cet exemple, nous avons créé un Router qui enveloppe notre application. 
-Nous avons également créé trois routes différentes pour les vues Home, About et Contact. 
-Chaque route est associée à un composant spécifique.
-Vous pouvez maintenant utiliser les composants de navigation fournis par React Router dans vos vues pour gérer la navigation entre elles.
-Par exemple, vous pouvez utiliser le composant Link pour créer un lien qui navigue vers une autre vue :
+Création de Routes :
+Définissez des routes en utilisant le composant Route de React Router. 
+Chaque route est associée à un chemin d'URL spécifique et un élément à afficher lorsque ce chemin est atteint. 
+Vous pouvez également utiliser la prop `render` pour rendre un composant avec des props supplémentaires ou de conditionner le rendu en fonction de certaines conditions.
 
-    import { Link } from 'react-router-dom';
-    function Home() {
-    return (
-        <div>
-        <h1>Home</h1>
-        <nav>
-            <ul>
-            <li>
-                <Link to="/about">About</Link>
+Utilisation des Composants de Navigation :
+React Router fournit des composants de navigation tels que Link et NavLink pour créer des liens vers d'autres vues. 
+Le composant NavLink peut être utilisé pour créer des liens de navigation avec une classe CSS active lorsque le lien est actif :
+
+import { Link, NavLink } from 'react-router-dom'; // Importation des composants de navigation de React Router
+
+function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <nav>
+        <ul>
+          <li>
+            <NavLink exact to="/">Home</NavLink> {// Utilisation de NavLink pour créer un lien vers la page d'accueil }
             </li>
             <li>
-                <Link to="/contact">Contact</Link>
+              <NavLink to="/about">About</NavLink> {// Utilisation de NavLink pour créer un lien vers la page "About" }
             </li>
-            </ul>
+            <li>
+              <NavLink to="/contact">Contact</NavLink> {// Utilisation de NavLink pour créer un lien vers la page "Contact" }
+            </li>
+          </ul>
         </nav>
-        </div>
+      </div>
     );
-    }
+  }
 
-    export default Home;
+Fonctionnalités Avancées :
+React Router offre des fonctionnalités avancées pour gérer la navigation comme la gestion des erreurs de routage. 
 
-Dans cet exemple, nous utilisons le composant Link pour créer des liens vers les vues About et Contact. 
-Lorsque l'utilisateur clique sur l'un de ces liens, React Router gère automatiquement la navigation vers la vue correspondante.
-C'est un exemple de base pour commencer à utiliser React Router dans vos applications React. 
-La bibliothèque offre de nombreuses fonctionnalités avancées pour gérer la navigation, 
-telles que la gestion des paramètres d'URL, la gestion des transitions d'écran, la gestion des erreurs de routage, etc. 
+ React Router est une bibliothèque essentielle pour gérer la navigation dans les applications React, et elle facilite la création de routes et de liens pour vos vues.
 */
