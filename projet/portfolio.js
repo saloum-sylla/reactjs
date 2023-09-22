f3a8dc0eed77b4bd5559223ec03ffccbfd364050
saloum-sylla <127609294+saloum-sylla@users.noreply.github.com>
2023-09-22 10:22:04 +0200

    Add files via upload

---
+++ projet/portfolio.js
@@ -0,0 +1,29 @@
+/**
+ 
+                             Site Portfolio en React
+Objectif
+Le but de ce projet est de créer un site portfolio personnel en utilisant React. 
+Le site portfolio devra présenter votre CV, vos projets et tout contenu que vous avez créé, le tout avec des images, des paragraphes et plusieurs pages.
+
+Exigences Techniques
+Utiliser React comme bibliothèque principale pour le développement du site web.
+Le projet doit utiliser des composants React pour organiser le contenu et les pages.
+Le site web doit être entièrement responsive.
+
+Fonctionnalités
+Le site portfolio devra comporter les sections suivantes :
+
+Page d'accueil : 
+    Une page d'accueil attrayante avec une brève introduction de l'auteur (vous), une image de profil et un lien vers le CV complet.
+
+CV : Une section présentant votre CV, avec des détails sur votre éducation, votre expérience professionnelle, vos compétences, etc.
+
+Projets : Une section pour présenter vos projets passés et actuels. 
+
+Contact : Une section qui affiche vos coordonnées.
+
+Conception
+Le site web doit avoir un design esthétique et professionnel. 
+Vous pouvez utiliser des bibliothèques CSS telles que Bootstrap pour simplifier la mise en page et la conception.
+
+ */
